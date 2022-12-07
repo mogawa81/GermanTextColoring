14c6af8514fb729b9d175396c88b803c8ffc1212
mogawa81 <78943577+mogawa81@users.noreply.github.com>
2022-12-06 20:00:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 DATABASE_URL = os.environ.get('DATABASE_URL')
-con = psycopg2.connect(DATABASE_URLsslmode='require')
+con = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = con.cursor()
 
 cur.execute('SELECT * FROM vocabulary')
