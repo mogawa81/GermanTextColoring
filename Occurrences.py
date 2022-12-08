bb7915040e684686bcb7e9fb242a9968e08ef636
mogawa81 <78943577+mogawa81@users.noreply.github.com>
2022-12-08 11:00:59 -0500

    Update Occurrences.py
    call nltk function not module

--- Occurrences.py
+++ Occurrences.py
@@ -51,7 +51,7 @@ def lemmatize(line):
         
 def extractProperNouns(text):
     tagger = ht.HanoverTagger('morphmodel_ger.pgz')
-    words = nltk.tokenize(text)
+    words = nltk.tokenize.word_tokenize(text)
     tokens=[word for (word,x,pos) in tagger.tag_sent(words,taglevel= 1) if pos == 'NE']
     return tokens
 
