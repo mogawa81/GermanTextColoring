719b0330f788269fa03d40b2e85e62eb1956db3d
mogawa81 <78943577+mogawa81@users.noreply.github.com>
2022-12-14 13:33:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from Occurrences import compileWords, lemmatize, extractProperNouns, readability
-from analyze_out import write_to_template
+#from analyze_out import write_to_template
 #import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect, abort, Blueprint
 import string
