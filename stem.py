be938b0b25c6ce6467dbde2c3d2b8ff9b61d3f27
mogawa81 <78943577+mogawa81@users.noreply.github.com>
2023-04-07 00:29:44 -0400

    Dependencies pt2

---
+++ stem.py
@@ -0,0 +1,21 @@
+from nltk.stem.snowball import GermanStemmer
+from compound_split import char_split
+import string
+
+#st = GermanStemmer()
+#token = "lieblingsband"
+#print(st.stem(token))
+
+
+#https://pypi.org/project/compound-split/ ~95% accuracy
+test_str = "Touristenmassen, die zu normalen Zeiten dort unterwegs sind schreckten mich eher ab. Auch mit Corona im Herbst 2020 war hier ziemlich viel los, es hielt sich aber in Grenzen und es waren meist Studenten und Einheimische und keine Reisegruppen. So wohnten wir zwei Nächte im Parkhotel Atlantic hoch über dem Neckar und fußläufig konnten wir von hier aus über den Schlosspark und vorbei am Heidelberger Schloss die berühmte Altstadt erreichen. Heidelberg ist aber nicht nur ein Touristenziel, die Stadt ist auch bekannt für ihre renommierte Universität. Die 1386 gegründete Ruperto Carola ist die älteste Universität im heutigen Deutschland und eine der forschungsstärksten in Europa. Sie zieht Wissenschaftler und Besucher aus aller Welt an und wird von 2019 an als eine von zehn Exzellenzuniversitäten und einem Exzellenzverbund in Deutschland gefördert."
+test_str = test_str.translate(str.maketrans('', '', string.punctuation))
+x = test_str.split()
+for word in x:
+    array = (char_split.split_compound(word)[0][0:])
+    if array[0] >= 0.6:
+        print(array)
+#print(char_split.split_compound('Autobahnraststätte')[0][1:])
+#print(char_split.split_compound('reise')[0][0])
+
+
