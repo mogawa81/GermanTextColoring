1270ffcf27530ab4a5a549f5a2f44e25c91a75ed
mogawa81 <78943577+mogawa81@users.noreply.github.com>
2023-04-07 00:47:47 -0400

    Revert "Tried spreading HanTa"
    This reverts commit bdc1539248d877d9e8f0247c45e0032befcbde51.

--- Occurrences.py
+++ Occurrences.py
@@ -4,8 +4,8 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 #import spacy
 import string
+from HanTa import HanoverTagger as ht
 #import sqlite3 as db
-from HanTa_methods import lemmatize, extractProperNouns
 import re
 import psycopg2
 import os
@@ -37,6 +37,21 @@ def compileWords(num):
         conn.commit()         
     conn.close()
     return wordBank
+
+def lemmatize(words):
+    mails_lemma = []
+    tagger = ht.HanoverTagger("morphmodel_ger.pgz")
+    #for mail in line.split():
+        #lemma = [lemma for (word,lemma,pos) in tagger.tag_sent(mail.split())]
+        #mails_lemma.append(' '.join(lemma))
+    mails_lemma = [lemma for (word, lemma, pos) in tagger.tag_sent(words)]
+    return mails_lemma
+        
+def extractProperNouns(words):
+    tagger = ht.HanoverTagger('morphmodel_ger.pgz')
+    tokens=[word for (word,x,pos) in tagger.tag_sent(words,taglevel= 1) if pos == 'NE']
+    tokens = list(dict.fromkeys(tokens))
+    return tokens
 
 def removeStopWords(words):
     stopWords = set(stopwords.words('german'))
